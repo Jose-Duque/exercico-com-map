1f28dc9988c5f267b633d7f22e41fcf15eabef92
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-02-15 18:42:29 -0300

    Entites class Eleição

---
+++ src/entities/Eleicao.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Eleicao {
+	
+	private String candidato;
+	private Integer votos;
+	public Eleicao(String candidato, Integer votos) {
+		super();
+		this.candidato = candidato;
+		this.votos = votos;
+	}
+	public String getCandidato() {
+		return candidato;
+	}
+	public void setCandidato(String candidato) {
+		this.candidato = candidato;
+	}
+	public Integer getVotos() {
+		return votos;
+	}
+	public void setVotos(Integer votos) {
+		this.votos = votos;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((candidato == null) ? 0 : candidato.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Eleicao other = (Eleicao) obj;
+		if (candidato == null) {
+			if (other.candidato != null)
+				return false;
+		} else if (!candidato.equals(other.candidato))
+			return false;
+		return true;
+	}
+	
+	
+}
